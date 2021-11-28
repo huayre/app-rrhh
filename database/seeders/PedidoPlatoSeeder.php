07ad97038480ea3113d7133b897bb41347afc427
Roy Carlos Huayre Bujaico <rhuayre@tumi-soft.com>
2021-11-28 18:07:39 -0500

    seeder

---
+++ database/seeders/PedidoPlatoSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PedidoPlato;
+use Illuminate\Database\Seeder;
+
+class PedidoPlatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       for ($i = 1; $i <= 3000 ;$i++) {
+           $cantidad = rand(1, 5);
+           PedidoPlato::create([
+               'pedido_id' =>$i++,
+               'plato_id'=>rand(1, 10),
+               'cantidad'=>$cantidad,
+               'precio' =>$cantidad * 12
+           ]);
+       }
+        for ($i = 0; $i <= 3000 ;$i++) {
+            $cantidad = rand(1, 5);
+            PedidoPlato::create([
+                'pedido_id' =>$i++,
+                'plato_id'=>rand(1, 10),
+                'cantidad'=>$cantidad,
+                'precio' =>$cantidad * 12
+            ]);
+        }
+    }
+}
