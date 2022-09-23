49d82c0f25f764a0c2ee8a87ef53232aedb077b0
Roy Carlos <rhuayre@tumi-soft.com>
2022-09-22 21:58:12 -0500

    mejora asistencia

---
+++ public/js/reporte.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#tabla-reposte-asistencia').DataTable(
+        {
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend:    'copyHtml5',
+                    text:      '<i class="fa fa-files-o"></i>',
+                    titleAttr: 'Copy'
+                },
+                {
+                    extend:    'excelHtml5',
+                    text:      '<i class="fa fa-file-excel-o text-success"></i>',
+                    titleAttr: 'Excel'
+                },
+                {
+                    extend:    'csvHtml5',
+                    text:      '<i class="fa fa-file-text-o text-primary"></i>',
+                    titleAttr: 'CSV'
+                },
+                {
+                    extend:    'pdfHtml5',
+                    text:      '<i class="fa fa-file-pdf-o text-danger"></i>',
+                    titleAttr: 'PDF'
+                }
+            ]
+        });
+});
+
