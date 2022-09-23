c228fb06c1ff107d9aba7ddc6706799474914e51
r0nald097 <ronaldo.albornoz17@gmail.com>
2022-09-22 19:17:57 -0500

    avance

--- database/seeders/AsistenciaSeeder.php
+++ database/seeders/AsistenciaSeeder.php
@@ -14,13 +14,13 @@ class AsistenciaSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=1; $i <= 2; $i++) { 
+        for ($i=1; $i <= 5; $i++) { 
             $dateInit = strtotime('2018-01-01');
             $dateEnd = strtotime(date('Y-m-d'));
             $asistencia = Asistencia::create([
                 'dia' => date('Y-m-d', mt_rand($dateInit, $dateEnd))
             ]);
-            for ($i=1; $i <= mt_rand(50, 100) ; $i++) { 
+            for ($j=1; $j <= mt_rand(50, 100) ; $j++) { 
                 $asistencia->personas()->attach(mt_rand(1, 100));    
             }
         }
